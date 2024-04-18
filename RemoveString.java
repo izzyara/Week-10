d582319aac0d11b4d3d48536c3c28b6eb4606d6d
izzyara <izzybzb@gmail.com>
2024-04-18 14:57:42 -0400

    Update RemoveString.java

--- RemoveString.java
+++ RemoveString.java
@@ -3,6 +3,12 @@ public class RemoveString
 {
     public static void main (String[]main)
     {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a sentence: ");
+        String sentence = input.next();
+        System.out.print("Enter a string: ");
+        String word = input.next();
+
         
     }
 }
