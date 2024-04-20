98d92aaf7b040a12b61116fde03ac7adec70eab9
izzyara <159071649+izzyara@users.noreply.github.com>
2024-04-19 21:02:56 -0400

    Update RemoveString.java

--- RemoveString.java
+++ RemoveString.java
@@ -5,10 +5,16 @@ public class RemoveString
     {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a sentence: ");
-        String sentence = input.next();
+        String sentence = input.nextLine();
         System.out.print("Enter a string: ");
         String word = input.next();
 
-        
+        if (sentence.contains(word)){
+            String newSentence = sentence.replace(word," ");
+            System.out.println(newSentence);
+        }else{
+            System.out.println("error");
+        }
+        input.close();
     }
 }
